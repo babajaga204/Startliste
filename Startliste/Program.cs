894b8fed43809eb4178da1d401f2d9414e6ec8a6
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-24 14:08:35 +0100

    Add project files.

---
+++ Startliste/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Startliste;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var registrationList = GetParticipants();
+
+    }
+
+    private static List<Registration> GetParticipants()
+    {
+        const string path = @"C:\Users\simen\github\Startliste\Startliste\startlist.csv";
+        var startlist = File.ReadLines(path, Encoding.UTF8);
+
+        var registrationList = startlist.Select(line => line.Split(",")).Select(participantInfo =>
+            new Registration(
+                participantInfo[0],
+                participantInfo[1],
+                participantInfo[2],
+                participantInfo[3],
+                participantInfo[4],
+                participantInfo[5])
+        ).ToList();
+        return registrationList;
+    }
+}
---
+++ Startliste/Registration.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Startliste
+{
+    internal class Registration
+    {
+        private string _startNumber;
+        private string _name;
+        private string _club;
+        private string _nationality;
+        private string _group;
+        private string _class;
+
+        public Registration(string startNumber, string name, string club, string nationality, string group, string @class)
+        {
+            _startNumber = startNumber;
+            _name = name;
+            _club = club;
+            _nationality = nationality;
+            _group = group;
+            _class = @class;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(
+                _startNumber.PadRight(15) 
+                + _name.PadRight(35)
+                + _club.PadRight(35)
+                + _nationality.PadRight(15) 
+                + _group.PadRight(50)
+                + _class.PadLeft(10)
+            );
+        }
+    }
+}
